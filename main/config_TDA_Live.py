bd86dac20cccc7750e55c33fff77f54a205d4fe8
Brandon <stdntuvmath1@outlook.com>
2025-04-23 23:33:43 -0500

    Update config_TDA_Live.py

--- main/config_TDA_Live.py
+++ main/config_TDA_Live.py
@@ -1,5 +1,5 @@
-token_path = 'C:\\Users\\stdnt\\Desktop\\Projects\\Software Projects\\Python\\LootLoader\\token.json'#place to put 90 day token
+token_path = 'C:\\Users\\removed\\Desktop\\Projects\\Software Projects\\Python\\LootLoader\\token.json'#place to put 90 day token
 redirect_url = 'http://localhost'#callback URL - must match EXACTLY what you put in your TDAmeritrage app registration
-api_key = 'AWINSEO1MT9VOGDXTHA5RLF8CZXJ9FLV'
-client_id = 'AWINSEO1MT9VOGDXTHA5RLF8CZXJ9FLV'
-account_id='255839651'
+api_key = 'removed'
+client_id = 'removed'
+account_id='removed'
